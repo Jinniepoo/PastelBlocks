eb81b61ccc9bcafbbbdb43acdc75348da1ce86cd
Jinniepoo <j.ambitious97@gmail.com>
2025-11-17 23:29:52 +0900

    SHape

--- Assets/Scipts/Editor/ShapeDataDrawer.cs
+++ Assets/Scipts/Editor/ShapeDataDrawer.cs
@@ -12,16 +12,78 @@ public class ShapeDataDrawer : Editor
 
     public override void OnInspectorGUI()
     {
-        
+        serializedObject.Update();
+        ClearBoardButton();
+        EditorGUILayout.Space();
+
+        DrawColumnsInputFields();
+        EditorGUILayout.Space();
+
+        if (ShapeDataInstance.board != null && ShapeDataInstance.cols > 0 && ShapeDataInstance.rows > 0)
+        {
+            DrawBoardTable();
+        }
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(ShapeDataInstance);
+        }
     }
 
     private void ClearBoardButton()
     {
-
+        if (GUILayout.Button("Clear Board"))
+        {
+            ShapeDataInstance.Clear();
+        }
     }
 
     private void DrawColumnsInputFields()
     {
+        var colsTmp = ShapeDataInstance.cols;
+        var rowsTmp = ShapeDataInstance.rows;
 
+        ShapeDataInstance.cols = EditorGUILayout.IntField("Columns", ShapeDataInstance.cols);
+        ShapeDataInstance.rows = EditorGUILayout.IntField("Rows", ShapeDataInstance.rows);
+
+        if ((ShapeDataInstance.cols != colsTmp || ShapeDataInstance.rows != rowsTmp) &&
+            ShapeDataInstance.cols > 0 && ShapeDataInstance.rows > 0)
+        {
+            ShapeDataInstance.CreateNewBoard();
+        }
+    }
+
+    private void DrawBoardTable()
+    {
+        var tableStyle = new GUIStyle("box");
+        tableStyle.padding = new RectOffset(10, 10, 10, 10);
+        tableStyle.margin.left = 32;
+
+        var headerColStyle = new GUIStyle();
+        headerColStyle.fixedWidth = 65;
+        headerColStyle.alignment = TextAnchor.MiddleCenter;
+
+        var rowStyle = new GUIStyle();
+        rowStyle.fixedHeight = 25;
+        rowStyle.alignment = TextAnchor.MiddleCenter;
+
+        var dataFieldStyle = new GUIStyle();
+        dataFieldStyle.normal.background = Texture2D.grayTexture;
+        dataFieldStyle.onNormal.background = Texture2D.whiteTexture;
+
+        for (var row = 0; row < ShapeDataInstance.rows; ++row)
+        {
+            EditorGUILayout.BeginHorizontal(headerColStyle);
+
+            for (var col = 0; col < ShapeDataInstance.cols; col++)
+            {
+                EditorGUILayout.BeginHorizontal(rowStyle);
+                var data = EditorGUILayout.Toggle(ShapeDataInstance.board[row].col[col], dataFieldStyle);
+                ShapeDataInstance.board[row].col[col] = data;
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
     }
 }
---
+++ Assets/Scipts/Shape/Shape.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public GameObject squareImage;
+
+    public ShapeData curShapeData;
+
+    private List<GameObject> currentShape = new List<GameObject>();
+    void Start()
+    {
+        RequestNewShape(curShapeData);
+    }
+
+    public void RequestNewShape(ShapeData shapeData)
+    {
+        CreateShape(shapeData);
+    }
+
+    public void CreateShape(ShapeData shapeData)
+    {
+        curShapeData = shapeData;
+
+        var totalSqNum = GetNumberOfSquares(shapeData);
+
+        while (currentShape.Count <= totalSqNum)
+        {
+            currentShape.Add(Instantiate(squareImage, transform) as GameObject);
+        }
+
+        foreach (var square in currentShape)
+        {
+            square.gameObject.transform.position = Vector3.zero;
+            square.gameObject.SetActive(false);
+        }
+
+        var squareRect = squareImage.GetComponent<RectTransform>();
+        var moveDist = new Vector2(squareRect.rect.width * squareRect.localScale.x,
+           squareRect.rect.height * squareRect.localScale.y);
+
+        int curIdx = 0; //list¿¡¼­
+        for (var row = 0; row < shapeData.rows; row++)
+        {
+            for (var col = 0; col < shapeData.cols; col++)
+            {
+                if (shapeData.board[row].col[col])
+                {
+                    currentShape[curIdx].SetActive(true);
+                    currentShape[curIdx].GetComponent<RectTransform>().localPosition = 
+                        new Vector2(GetXPositionForShapeSquare(shapeData, col, moveDist),
+                        GetYPositionForShapeSquare(shapeData, row, moveDist));
+
+                    curIdx++;
+                }
+            }
+        }
+
+    }
+    private float GetYPositionForShapeSquare(ShapeData shapeData, int row, Vector2 moveDist)
+    {
+        float shiftOnY = 0f;
+
+        if (shapeData.rows > 1)
+        {
+            if (shapeData.rows % 2 != 0)
+            {
+                var midSquareIdx = (shapeData.rows - 1) / 2;
+                var multiplier = (shapeData.rows - 1) / 2;
+
+                if (row < midSquareIdx)
+                {
+                    shiftOnY = moveDist.y * 1;
+                    shiftOnY *= multiplier;
+                }
+                else if (row > midSquareIdx)
+                {
+                    shiftOnY = moveDist.y * -1;
+                    shiftOnY *= multiplier;
+                }
+            }
+            else
+            {
+                var midSquareIdx2 = (shapeData.rows == 2) ? 1 : (shapeData.rows / 2);
+                var midSquareIdx1 = (shapeData.rows == 2) ? 0 : (shapeData.rows - 2);
+                var multiplier = shapeData.rows / 2;
+
+                if (row == midSquareIdx1 || row == midSquareIdx2)
+                {
+                    if (row == midSquareIdx2)
+                        shiftOnY = (moveDist.y / 2) * -1;
+
+                    if (row == midSquareIdx1)
+                        shiftOnY = (moveDist.y / 2);
+                }
+                if (row < midSquareIdx1 && row < midSquareIdx2)
+                {
+                    shiftOnY = moveDist.x * -1;
+                    shiftOnY *= multiplier;
+                }
+                else if (row > midSquareIdx1 && row > midSquareIdx2)
+                {
+                    shiftOnY = moveDist.x * 1;
+                    shiftOnY *= multiplier;
+                }
+            }
+        }
+
+        return shiftOnY;
+    }
+
+    private float GetXPositionForShapeSquare(ShapeData shapeData, int col, Vector2 moveDist)
+    {
+        float shiftOnX = 0f;
+
+        if (shapeData.cols > 1)
+        {
+            if (shapeData.cols % 2 != 0)
+            {
+                var middleSquareIdx = (shapeData.cols - 1) / 2;
+                var multiplier = (shapeData.cols - 1) / 2;
+                if (col < middleSquareIdx)
+                {
+                    shiftOnX = moveDist.x * -1;
+                    shiftOnX *= multiplier;
+                }
+                else if (col > middleSquareIdx)
+                {
+                    shiftOnX = moveDist.x * 1;
+                    shiftOnX *= multiplier;
+                }
+            }
+            else
+            {
+                var middleSquareIdx2 = (shapeData.cols == 2) ? 1 : (shapeData.cols / 2);
+                var middleSquareIdx1 = (shapeData.cols == 2) ? 0 : (shapeData.cols - 1);
+                var multiplier = shapeData.cols / 2;
+
+                if (col == middleSquareIdx2 || col == middleSquareIdx1)
+                {
+                    if (col == middleSquareIdx2)
+                        shiftOnX = moveDist.x / 2;
+                    if (col == middleSquareIdx1)
+                        shiftOnX = (moveDist.x / 2) * -1;
+                }
+
+                if (col < middleSquareIdx1 && col < middleSquareIdx2)
+                {
+                    shiftOnX = moveDist.x * -1;
+                    shiftOnX *= multiplier;
+                }
+                else if (col > middleSquareIdx1 && col > middleSquareIdx2)
+                {
+                    shiftOnX = moveDist.x * 1;
+                    shiftOnX *= multiplier;
+                }
+            }
+        }
+        return shiftOnX;
+    }
+
+    private int GetNumberOfSquares(ShapeData shapeData)
+    {
+        int num = 0;
+
+        foreach (var rowData in shapeData.board)
+        {
+            foreach (var active in rowData.col)
+            {
+                if (active)
+                    num++;
+            }
+        }
+        return num;
+    }
+
+}
